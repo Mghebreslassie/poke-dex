a27bc7908943e86c0b62a3b7380c02580d5b561a
mghebreslassie <messeleghebreslassie@gmail.com>
2022-01-17 20:33:02 -0700

    moved file to diff dir.

--- pages/pokemon/[name].tsx
+++ pages/pokemon/[name].tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { PokeResults } from "..";
-import { Pokedex, Pokemon } from "../types";
+import { Pokedex, Pokemon } from "../../types";
 
 const PokePage: NextPage<{ data: Pokemon; desc: Pokedex }> = ({
   data,
